61d6a0da4bad0068065ce7b9d48fc72a19c02377
Chris Jessup <chris.jessup@hellofresh.com.au>
2022-03-21 09:11:47 +1100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # backend-task
-A task for backend developers
+
+A task for backend developers.
+
+This is small program with a logical error in the implementation of some logic.
+
+This is meant to demonstrate some familiarity with:
+* Git/Github
+* Python
+* Setting up an environment to run/test a program
+* Working with data (in this case two csv files)
+* Identifying logical errors
+
+## Steps for a solution
+
+1. Work out how to download/clone the repo
+2. Work out how to run the program (all the files necessary are in this repo)
+3. Look at any documentation to work out the correct operation
+4. Compare the actual output with any output
+5. Identify the bug
+6. (optional) Fix the bug!
+7. Identify how to avoid this bug in future.
+
+## Things we do and don't care about
+We don't necessarily care that you are able to solve the bug in the program, we are more interested in your ability
+to demonstrate familiarity with the tools of the trade
---
+++ common_elements.py
@@ -0,0 +1,45 @@
+import csv
+import sys
+
+def common_ids_with_bug(left, right):
+    """ 
+
+    Given two lists dictionaries with the following shape:
+        {
+            'id':number,
+            'name':string
+        }
+    return a tuple of all the IDs that are common to both lists
+
+    Warning: This version has a bug!!!
+        1. What is the bug?
+        2. How would you solve it?
+        3. How would you test it?
+
+    """
+
+    common = []
+    for element in left:
+        if element in right:
+            common.append(element)
+
+    common = [x['id'] for x in common]
+
+    return common
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 3:
+        with open(sys.argv[1]) as fp:
+            left_input = list(csv.DictReader(fp))
+        with open(sys.argv[2]) as fp:
+            right_input = list(csv.DictReader(fp))
+    else:
+        print(f"USAGE: {sys.argv[0]} <left.csv> <right.csv>", file=sys.stderr)
+        sys.exit(1)
+
+    common_elements = common_ids_with_bug(left_input, right_input)
+    print(f"IDs that are common to '{sys.argv[1]}' and '{sys.argv[2]}'")
+    for element in common_elements:
+        print(f"    {element}")
